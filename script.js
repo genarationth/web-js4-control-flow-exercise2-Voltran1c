97d7df9844d76b051f89dbde620a21e07c3c01ca
Watcharapong.K <147409902+Voltran1c@users.noreply.github.com>
2024-05-14 13:49:38 +0700

    Js if-else2 commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const dayNumber = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const dayIndex = 5;
+switch (dayIndex) {
+  case 0:
+    console.log("This is " + dayNumber[0]);
+    break;
+  case 1:
+    console.log("This is " + dayNumber[1]);
+    break;
+  case 2:
+    console.log("This is " + dayNumber[2]);
+    break;
+  case 3:
+    console.log("This is " + dayNumber[3]);
+    break;
+  case 4:
+    console.log("This is " + dayNumber[4]);
+    break;
+  case 5:
+    console.log("This is " + dayNumber[5]);
+    break;
+  case 6:
+    console.log("This is " + dayNumber[6]);
+    break;
+  default:
+    console.log("Not day of the week");
+}
